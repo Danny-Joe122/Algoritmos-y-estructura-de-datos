b18cd89ae598b954b8c28e920a26950d98896f7f
Danny-Joe122 <danny.carlosh@estudiante.ucsm.edu.pe>
2025-04-12 23:21:31 -0500

    Create Actividad_3_Binario.java

---
+++ Laboratorio_4/Actividades/Actividad_3.1.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+public class BinarySearch {
+
+    public int binarySearch(int[] arr, int lo, int hi, int x) {
+        if (hi >= lo && lo < arr.length) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] == x)
+                return mid;
+
+            if (arr[mid] > x)
+                return binarySearch(arr, lo, mid - 1, x);
+
+            return binarySearch(arr, mid + 1, hi, x);
+        }
+        return -1; // No encontrado
+    }
+
+    public static void main(String[] args) {
+        BinarySearch ob = new BinarySearch();
+        int[] arr = {1, 2, 3, 4, 5};
+        int x = 4;
+        int position = ob.binarySearch(arr, 0, arr.length - 1, x);
+
+        if (position == -1)
+            System.out.println("Elemento no encontrado!!!");
+        else
+            System.out.println("Elemento encontrado en el índice: " + position);
+    }
+}
