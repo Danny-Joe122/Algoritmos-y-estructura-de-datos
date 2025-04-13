fd130ebb6cdb18dd0a2a906a43c732de7caf7152
Danny-Joe122 <danny.carlosh@estudiante.ucsm.edu.pe>
2025-04-12 23:36:55 -0500

    Update Actividad_5.java

--- Laboratorio_4/Actividades/Actividad_5.java
+++ Laboratorio_4/Actividades/Actividad_5.java
@@ -1,22 +1,29 @@
-package NaiveSolution;
+public class naiveSolution { 
+ 	static int getValue(int[] values, int length) 
+ 	{  	if (length <= 0) 
+ 	 	 	return 0; 
+ 	 	int tmpMax = -1; 
+        for (int i = 0; i < length; i++) { 
+ 	 	 	tmpMax = Math.max(tmpMax, values[i] + getValue(values, length - i - 1)); 
+ 	 	} 
+ 	 	return tmpMax; 
+ 	} 
+ 	public static void main(String[] args)  	{  	int[] values = new int[]{3, 7, 1, 3, 9};   	 	int rodLength = values.length; 
+  System.out.println("El valor maximo:" + getValue(values, rodLength)); 
+ 	} 
+} 
 
-public class NaiveSolution {
-
-    static int getValue(int[] values, int length) {
-        if (length <= 0)
-            return 0;
-
-        int tmpMax = -1;
-        for (int i = 0; i < length; i++) {
-            tmpMax = Math.max(tmpMax, values[i] + getValue(values, length - i - 1));
-        }
-        return tmpMax;
-    }
-
-    public static void main(String[] args) {
-        int[] values = {3, 7, 1, 3, 9}; // Precios por longitud 1 a 5
-        int rodLength = values.length;
-
-        System.out.println("El valor máximo (naive): " + getValue(values, rodLength));
-    }
-}
+public class dpSolution { 
+ 	static int getValue(int[] values, int rodLength)  	{  	int[] subSolutions = new int[rodLength + 1];  	 	for (int i = 1; i <= rodLength; i++) 
+ 	 	{  	int tmpMax = -1; 
+ 	 	for (int j = 0; j < i; j++) 
+ 	 	 	tmpMax = Math.max(tmpMax, values[j] + subSolutions[i - j - 
+1]); 
+ 	 	 	subSolutions[i] = tmpMax; 
+ 	 	} 
+ 	 	return subSolutions[rodLength]; 
+ 	} 
+ 	public static void main(String[] args)  	{  	int[] values = new int[]{1, 7, 1, 3, 9};  	 	int rodLength = values.length; 
+  System.out.println("El valor maximo: " + getValue(values, rodLength)); 
+ 	} 
+} 
