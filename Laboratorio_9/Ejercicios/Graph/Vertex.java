5497b16ec55ac45d26929ec2ae12d5067009cfa1
Danny-Joe122 <danny.carlosh@estudiante.ucsm.edu.pe>
2025-06-08 19:37:47 -0500

    Create Vertex.java

---
+++ Laboratorio_9/Ejercicios/Graph/Vertex.java
@@ -0,0 +1,26 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+    private String name;
+    private List<Edge> edges;
+
+    public Vertex(String name) {
+        this.name = name;
+        edges = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addEdge(Edge edge) {
+        edges.add(edge);
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+}
